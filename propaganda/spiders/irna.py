ffd38bcf662af6a7d83e0262cb3e69f2e58ad765
Jonah Lubin <E31506@sri.com>
2017-08-23 12:13:07 -0400

    first commit

---
+++ propaganda/spiders/irna.py
@@ -0,0 +1,29 @@
+import scrapy
+
+class IrnaSpider(scrapy.Spider):
+    name = 'irna'
+    base_url = 'http://www.irna.ir/en/services/161'
+    next_page = 162
+
+    def start_requests(self):
+        yield scrapy.Request(self.base_url, meta={'page_number': 1})
+
+    def parse(self, response):
+        for article_url in response.css('.DataListContainer h3 a::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(article_url), callback=self.parse_article)
+
+        # page_number = response.meta['page_number'] + 1
+        # if response.css('#MoreButton'):
+        #     yield scrapy.Request('{}/page{}'.format(self.base_url, page_number),
+        #         callback=self.parse, meta={'page_number': page_number})
+
+
+        for next_article in ('/en/services/162/', '/en/services/163/', '/en/services/164/'):
+            yield response.follow(next_article, callback=self.parse)
+
+    def parse_article(self, response):
+        yield {
+            'title': ' '.join(response.xpath('//*[@id="col-3"]/div/div[1]/div/h1/text()').extract_first()),
+            'text': ' '.join(response.xpath('//p[@id="ctl00_ctl00_ContentPlaceHolder_ContentPlaceHolder_NewsContent4_BodyLabel"]/text()').extract()),
+            'tags': [tag.strip() for tag in response.xpath('//div[@class="Tags"]/p/a/text()').extract() if tag.strip()]
+        }
---
+++ propaganda/spiders/sputnik.py
@@ -0,0 +1,27 @@
+import scrapy
+
+class SputnikSpider(scrapy.Spider):
+    name = 'sputnik'
+    base_url = 'https://sputniknews.com/world/'
+
+    def start_requests(self):
+        yield scrapy.Request(self.base_url, meta={'page_number': 1})
+
+    def parse(self, response):
+        for article_url in response.css('.b-stories__title h2 a::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(article_url), callback=self.parse_article)
+
+        more_button = response.xpath('//*[@id="rubric-major"]/div[2]/a/@href').extract_first()
+        if more_button is not None:
+            yield response.follow(more_button, callback=self.parse)
+
+        for next_article in ('/business/', '/politics/', '/analysis/', '/art_living/', '/science/'):
+            yield response.follow(next_article, callback=self.parse)
+
+
+    def parse_article(self, response):
+        yield {
+            'title': ' '.join(response.xpath('/html/body/div[7]/div/div/div[1]/h1/text()').extract_first()),
+            'text': ' '.join(response.xpath('/html/body/div[8]/div[1]/div/div[3]/p/text()').extract()),
+            'tags': [tag.strip() for tag in response.xpath('/html/body/div[8]/div[1]/div/div[9]/div[3]/div/div[2]').re(r'>(\S+)</a>') if tag.strip()]
+        }
